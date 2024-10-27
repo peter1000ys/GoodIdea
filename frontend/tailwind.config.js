945bfdb49af74c96b2a5ae72d3d8a3d5a1a4e7ea
zxxv1245 <150820275+zxxv1245@users.noreply.github.com>
2024-10-27 20:41:37 +0900

    feat: 백그라운드 커스텀 색깔 추가

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,7 +3,16 @@ export default {
   mode: "jit",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        grayCustom: {
+          100: "#999999", // 8%
+          200: "#717171", // 32%
+          300: "#666666", // 53%
+          400: "#333333", // 83%
+        },
+      },
+    },
   },
   plugins: [],
 };
