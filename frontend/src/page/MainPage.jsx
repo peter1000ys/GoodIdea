c72e63c489e496cdda6eb9778251ee93ca78370d
이주호 <zxxv1245@naver.com>
2024-10-28 16:21:48 +0900

    feat: 깃랩 로그인 버튼

--- frontend/src/page/MainPage.jsx
+++ frontend/src/page/MainPage.jsx
@@ -1,12 +1,22 @@
 import { Helmet } from "react-helmet-async";
 
 function MainPage() {
+  // 리다이렉트 URL 설정
+  const handleGitLabLogin = () => {
+    window.location.href =
+      "https://lab.ssafy.com/oauth/authorize?client_id=423f3efe4f264ff88416dc5ad049498edfaeaf5a68dcdb835ee4ce5b0bf48f32&redirect_uri=http://oracle1.mypjt.xyz/api/v1/hello/callback&response_type=code&scope=read_user%20api&state=random_state_string";
+  };
+
   return (
     <>
       <Helmet>
         <title>메인페이지</title>
       </Helmet>
-      메인페이지
+      <div>
+        메인페이지
+        {/* 버튼 추가 */}
+        <button onClick={handleGitLabLogin}>GitLab로 로그인</button>
+      </div>
     </>
   );
 }
