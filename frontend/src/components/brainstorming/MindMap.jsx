7a772e4534c37938f14e5340bf4a002b7cd8424d
한세훈 <atdark04@gmail.com>
2024-10-28 11:16:05 +0900

    feat: 마인드맵 페이지 검색/인포/UI/레이아웃

--- frontend/src/components/brainstorming/MindMap.jsx
+++ frontend/src/components/brainstorming/MindMap.jsx
@@ -1,82 +1,9 @@
 import { useCallback, useEffect } from "react";
 import { useRef } from "react";
 import * as d3 from "d3";
+// import { mindMapColorData, mindMapData } from "../../dummy/brainstorming";
 
-const data = {
-  nodes: [
-    { id: "미국", category: "center" },
-    { id: "인도", category: "blue" },
-    { id: "강화", category: "yellow" },
-    { id: "해외", category: "coral" },
-    { id: "안정적", category: "green" },
-    { id: "차량", category: "blue" },
-    { id: "소비자", category: "lightblue" },
-    { id: "경기변동", category: "blue" },
-    { id: "공장", category: "blue" },
-    { id: "유럽", category: "coral" },
-    { id: "자동차", category: "turquoise" },
-    { id: "임상", category: "turquoise" },
-    { id: "시장점유율", category: "turquoise" },
-    { id: "치료제", category: "blue" },
-    { id: "일본", category: "coral" },
-    { id: "국내", category: "coral" },
-    { id: "중국", category: "coral" },
-    { id: "가다", category: "yellow" },
-    { id: "정책", category: "green" },
-    { id: "바이오", category: "turquoise" },
-    { id: "국가", category: "green" },
-    { id: "한국", category: "coral" },
-    { id: "증가", category: "blue" },
-    { id: "연구개발", category: "yellow" },
-    { id: "독일", category: "lightblue" },
-    { id: "시험", category: "blue" },
-    { id: "의약품", category: "blue" },
-    { id: "아시아", category: "green" },
-    { id: "시장", category: "coral" },
-  ],
-  links: [
-    { source: "미국", target: "인도" },
-    { source: "미국", target: "강화" },
-    { source: "미국", target: "해외" },
-    { source: "미국", target: "안정적" },
-    { source: "미국", target: "차량" },
-    { source: "미국", target: "소비자" },
-    { source: "미국", target: "경기변동" },
-    { source: "미국", target: "공장" },
-    { source: "미국", target: "유럽" },
-    { source: "미국", target: "자동차" },
-    { source: "미국", target: "임상" },
-    { source: "미국", target: "시장점유율" },
-    { source: "미국", target: "치료제" },
-    { source: "미국", target: "일본" },
-    { source: "미국", target: "국내" },
-    { source: "미국", target: "중국" },
-    { source: "미국", target: "가다" },
-    { source: "미국", target: "정책" },
-    { source: "미국", target: "바이오" },
-    { source: "미국", target: "국가" },
-    { source: "미국", target: "한국" },
-    { source: "미국", target: "증가" },
-    { source: "미국", target: "연구개발" },
-    { source: "미국", target: "독일" },
-    { source: "미국", target: "시험" },
-    { source: "미국", target: "의약품" },
-    { source: "미국", target: "아시아" },
-    { source: "미국", target: "시장" },
-  ],
-};
-
-const colorMap = {
-  center: "#333333",
-  blue: "#4B89DC",
-  yellow: "#F1C40F",
-  coral: "#FF7473",
-  green: "#2ECC71",
-  lightblue: "#5DADE2",
-  turquoise: "#1ABC9C",
-};
-
-const MindMap = ({ setSelectedKeyword }) => {
+const MindMap = ({ setSelectedKeyword, mindMapData, mindMapColorData }) => {
   const svgRef = useRef();
   const clickHandle = useCallback(
     (event, d) => {
@@ -102,11 +29,11 @@ const MindMap = ({ setSelectedKeyword }) => {
 
     // 시뮬레이션 설정
     const simulation = d3
-      .forceSimulation(data.nodes)
+      .forceSimulation(mindMapData.nodes)
       .force(
         "link",
         d3
-          .forceLink(data.links)
+          .forceLink(mindMapData.links)
           .id((d) => d.id)
           .distance(100)
       )
@@ -118,7 +45,7 @@ const MindMap = ({ setSelectedKeyword }) => {
     const link = svg
       .append("g")
       .selectAll("line")
-      .data(data.links)
+      .data(mindMapData.links)
       .join("line")
       .style("stroke", "#999")
       .style("stroke-width", 0.5);
@@ -127,7 +54,7 @@ const MindMap = ({ setSelectedKeyword }) => {
     const node = svg
       .append("g")
       .selectAll("g")
-      .data(data.nodes)
+      .data(mindMapData.nodes)
       .join("g")
       .call(
         d3
@@ -142,7 +69,7 @@ const MindMap = ({ setSelectedKeyword }) => {
       .append("rect")
       .attr("rx", 6)
       .attr("ry", 6)
-      .attr("fill", (d) => colorMap[d.category])
+      .attr("fill", (d) => mindMapColorData[d.category])
       .style("cursor", "pointer")
       .attr("stroke", "white")
       .attr("stroke-width", 2)
--- frontend/src/page/projectdetail/MindMapPage.jsx
+++ frontend/src/page/projectdetail/MindMapPage.jsx
@@ -1,10 +1,60 @@
 import { Helmet } from "react-helmet-async";
 import MindMap from "../../components/brainstorming/MindMap";
 import Header from "../../components/common/Header";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import {
+  mindMapColorData,
+  mindMapData as mmdata,
+} from "../../dummy/brainstorming";
 
 function MindMapPage() {
+  const [searchKeyword, setSearchKeyword] = useState("");
   const [selectedKeyword, setSelectedKeyword] = useState(null);
+  const [selectedDetail, setSelectedDetail] = useState(null);
+  const [mindMapData, setMindMapData] = useState({
+    nodes: [],
+    links: [],
+  });
+
+  useEffect(() => {
+    // 스크롤 애니메이션 효과
+    if (selectedDetail) {
+      const detailElement = document.getElementById(
+        `detail-${selectedDetail.id}`
+      );
+      if (detailElement) {
+        detailElement.scrollIntoView({ behavior: "smooth", block: "center" });
+      }
+    }
+  }, [selectedDetail]);
+
+  const handleSearch = () => {
+    const keyword = searchKeyword.trim();
+    if (!keyword) {
+      alert("검색어를 입력해주세요.");
+      return;
+    }
+    setSelectedDetail(null);
+    setSearchKeyword("");
+
+    setSelectedKeyword(keyword);
+    setMindMapData(mmdata);
+
+    alert("검색어가 입력되었습니다: " + keyword);
+  };
+
+  const handleDetailClick = (detail) => {
+    if (selectedDetail && selectedDetail.id === detail.id) {
+      setSelectedDetail(null);
+      return;
+    }
+    setSelectedDetail(detail);
+  };
+
+  const handeInfoClick = () => {
+    alert("인포아이콘클릭");
+  };
+
   return (
     <>
       <Helmet>
@@ -16,41 +66,115 @@ function MindMapPage() {
         {/* 검색창 */}
         <div className="flex justify-center my-8">
           <input
+            id="searchInput"
             type="text"
+            onKeyDown={(e) =>
+              e.key === "Enter" ? handleSearch(e.target.value) : null
+            }
+            onChange={(e) => setSearchKeyword(e.target.value)}
+            value={searchKeyword}
+            autoComplete="off"
             placeholder="여기에 검색어를 입력하세요"
             className="w-full max-w-2xl p-3 border border-gray-300 rounded-full text-center shadow-sm focus:outline-none"
           />
-          <button className="ml-3 p-3 bg-gray-200 rounded-full shadow-sm">
-            🔍
-          </button>
-          <button className="ml-3 p-3 bg-gray-200 rounded-full shadow-sm">
-            ℹ️
-          </button>
+
+          {/* 돋보기 아이콘 */}
+          <div
+            onClick={handleSearch}
+            className="ml-3 p-3 cursor-pointer bg-gray-100 rounded-full shadow-sm"
+          >
+            <svg
+              viewBox="0 0 1024 1024"
+              fill="currentColor"
+              height="2em"
+              width="2em"
+            >
+              <path d="M909.6 854.5L649.9 594.8C690.2 542.7 712 479 712 412c0-80.2-31.3-155.4-87.9-212.1-56.6-56.7-132-87.9-212.1-87.9s-155.5 31.3-212.1 87.9C143.2 256.5 112 331.8 112 412c0 80.1 31.3 155.5 87.9 212.1C256.5 680.8 331.8 712 412 712c67 0 130.6-21.8 182.7-62l259.7 259.6a8.2 8.2 0 0011.6 0l43.6-43.5a8.2 8.2 0 000-11.6zM570.4 570.4C528 612.7 471.8 636 412 636s-116-23.3-158.4-65.6C211.3 528 188 471.8 188 412s23.3-116.1 65.6-158.4C296 211.3 352.2 188 412 188s116.1 23.2 158.4 65.6S636 352.2 636 412s-23.3 116.1-65.6 158.4z" />
+            </svg>
+          </div>
+
+          {/* 인포 아이콘 */}
+          <div
+            onClick={handeInfoClick}
+            className="ml-3 p-3 cursor-pointer bg-gray-100 rounded-full shadow-sm"
+          >
+            <svg
+              viewBox="0 0 1024 1024"
+              fill="currentColor"
+              height="2em"
+              width="2em"
+            >
+              <path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm0 820c-205.4 0-372-166.6-372-372s166.6-372 372-372 372 166.6 372 372-166.6 372-372 372z" />
+              <path d="M464 336a48 48 0 1096 0 48 48 0 10-96 0zm72 112h-48c-4.4 0-8 3.6-8 8v272c0 4.4 3.6 8 8 8h48c4.4 0 8-3.6 8-8V456c0-4.4-3.6-8-8-8z" />
+            </svg>
+          </div>
         </div>
 
         {/* 컨텐츠 레이아웃 */}
         <div className="flex justify-center">
+          {/* 컨텐츠 영역 */}
           <div className="w-full max-w-5xl flex bg-white shadow-lg rounded-lg border overflow-hidden">
-            <MindMap setSelectedKeyword={setSelectedKeyword} />
+            {mindMapData?.nodes?.length === 0 ? (
+              // 미입력 문구
+              <div className="flex w-full items-center justify-center bg-gray-50 p-10">
+                <div className="text-center">
+                  <h2 className="text-2xl font-semibold text-gray-600 mb-4">
+                    아직 검색어가 입력되지 않았습니다
+                  </h2>
+                  <p className="text-gray-500 mb-6">
+                    보고 싶은 키워드를 입력해 관련된 뉴스를 확인해보세요!
+                  </p>
+                  <button
+                    onClick={() =>
+                      document.getElementById("searchInput").focus()
+                    }
+                    className="px-6 py-3 bg-blue-500 text-white rounded-lg shadow hover:bg-blue-600 transition"
+                  >
+                    검색어 입력하기
+                  </button>
+                </div>
+              </div>
+            ) : (
+              // 마인드맵 영역
+              <>
+                <MindMap
+                  setSelectedKeyword={setSelectedKeyword}
+                  mindMapColorData={mindMapColorData}
+                  mindMapData={mindMapData}
+                />
+                {/* 관련 뉴스 리스트 영역 */}
+                <div className="w-1/3 p-6 bg-blue-100 border-l border-gray-300">
+                  <h2 className="text-center font-bold mb-4">
+                    ← 키워드와 관련된 뉴스 →
+                  </h2>
+                  <div className="space-y-2 text-gray-700">
+                    {mindMapData.nodes?.map((item, index) => (
+                      <div key={item.id}>
+                        <div
+                          id={`detail-${item.id}`}
+                          onClick={() => handleDetailClick(item)}
+                          className="cursor-pointer"
+                        >
+                          {index + 1}. {item.id}
+                        </div>
 
-            {/* 관련 뉴스 리스트 영역 */}
-            <div className="w-1/3 p-6 bg-blue-100 border-l border-gray-300">
-              <h2 className="text-center font-bold mb-4">
-                ← 키워드와 관련된 뉴스 →
-              </h2>
-              <ul className="space-y-2 text-gray-700">
-                <li>1. 키워드</li>
-                <li>2. 키워드</li>
-                <li>3. 키워드</li>
-                <li>4. 키워드</li>
-                <li>5. 키워드</li>
-                <li>6. 키워드</li>
-                <li>7. 키워드</li>
-                <li>8. 키워드</li>
-                <li>9. 키워드</li>
-                <li>10. 키워드</li>
-              </ul>
-            </div>
+                        {/* 하단에 선택 시에 무슨 정보가?... */}
+                        {selectedDetail === item && (
+                          <div className="ml-4">
+                            <li>키워드 : {item.id}</li>
+                            <li>키워드 : {item.id}</li>
+                            <li>키워드 : {item.id}</li>
+                            <li>키워드 : {item.id}</li>
+                            <li>키워드 : {item.id}</li>
+                            <li>키워드 : {item.id}</li>
+                          </div>
+                        )}
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              </>
+            )}
           </div>
         </div>
       </div>
