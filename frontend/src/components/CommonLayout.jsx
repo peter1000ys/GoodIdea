b1d3bf26ae301fbba775080469ed53afbbf5455a
이주호 <zxxv1245@naver.com>
2024-10-28 17:13:01 +0900

    feat: Nav 안쓰는 페이지에서 안 나오게 함

--- frontend/src/components/CommonLayout.jsx
+++ frontend/src/components/CommonLayout.jsx
@@ -1,14 +1,20 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Outlet, useLocation } from "react-router-dom";
 import Nav from "./common/Nav";
 
 function CommonLayout() {
+  const localtion = useLocation();
+  const [isProjectDetail, setisProjectDetail] = useState(false);
+  useEffect(() => {
+    setisProjectDetail(location.pathname.includes("projectdetail"));
+  }, [localtion.pathname]);
+
   return (
     <div className="flex min-h-screen min-w-screen">
-      <Nav />
+      {isProjectDetail && <Nav />}
 
       {/* Main Content */}
-      <main className="flex-1 ml-64">
+      <main className={`flex-1 ${isProjectDetail ? "ml-64" : "ml-0"}`}>
         {/* 자식 컴포넌트를 렌더링 */}
         <Outlet />
       </main>
