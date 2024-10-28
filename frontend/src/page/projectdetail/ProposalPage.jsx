1c0a7c283856a2a1e245f4471687ed30b31db512
한세훈 <atdark04@gmail.com>
2024-10-28 16:46:42 +0900

    feat: 프로젝트 개요 페이지 세팅

--- frontend/src/page/projectdetail/ProposalPage.jsx
+++ frontend/src/page/projectdetail/ProposalPage.jsx
@@ -1,16 +1,23 @@
 import { Helmet } from "react-helmet-async";
 import Header from "../../components/common/Header";
+import DefaultButton from "../../components/common/DefaultButton";
 
 function ProposalPage() {
   return (
     <>
       <Helmet>
-        <title>기획서페이지</title>
+        <title>프로젝트 개요</title>
       </Helmet>
       <div className="h-full w-full flex flex-col">
         <Header content="관통 프로젝트" />
 
-        <div>프로젝트 개요</div>
+        <div className="flex-1 items-center justify-center flex">CRDT 영역</div>
+        <div>
+          <div className="float-end space-x-3 p-2">
+            <DefaultButton text="팀명 수정" />
+            <DefaultButton text="저장" />
+          </div>
+        </div>
       </div>
     </>
   );
