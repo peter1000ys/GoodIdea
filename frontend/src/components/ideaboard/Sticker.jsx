8ee8ab59f5b84e34dc9e331ed01a7b697e9ce4bf
이주호 <zxxv1245@naver.com>
2024-10-30 11:50:49 +0900

    feat: 스티커가 날아온당

--- frontend/src/components/ideaboard/Sticker.jsx
+++ frontend/src/components/ideaboard/Sticker.jsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
 
-const Sticker = ({ delay }) => {
+const Sticker = ({ delay, x, y, color, darkColor }) => {
   const [animate, setAnimate] = useState(false);
+
   useEffect(() => {
     // delay 시간 후에 애니메이션 활성화
     const timer = setTimeout(() => setAnimate(true), delay);
@@ -10,26 +11,36 @@ const Sticker = ({ delay }) => {
 
   return (
     <div
-      className={`relative w-72 h-72 transform transition-transform duration-700 ${
-        animate ? "translate-y-0 opacity-100" : "translate-y-20 opacity-0"
+      className={`absolute w-72 h-72 ${
+        animate ? "animate-tinDownIn" : "opacity-0"
       }`}
-      style={{ transitionDelay: `${delay}ms` }}
+      style={{
+        left: x,
+        top: y,
+        transitionDelay: `${delay}ms`,
+      }}
     >
       {/* 상단 메모 부분 */}
-      <div className="w-72 h-60 bg-blue-100 rounded-tr-[30px] rounded-tl-[30px]">
+      <div
+        className="w-72 h-60 rounded-tr-[30px] rounded-tl-[30px]"
+        style={{ backgroundColor: color }}
+      >
         <div className="p-4">
           <p className="text-gray-700">메모 내용을 입력하세요...</p>
         </div>
       </div>
 
       {/* 하단 바 부분 */}
-      <div className="left-0 w-60 h-12 bg-blue-100 ml-auto mr-0"></div>
+      <div
+        className="left-0 w-60 h-12 ml-auto mr-0"
+        style={{ backgroundColor: color }}
+      ></div>
 
       {/* 왼쪽 하단 접힌 부분 */}
       <div
         className="absolute bottom-0 left-0 w-12 h-12"
         style={{
-          backgroundColor: "#a2d2e9",
+          backgroundColor: darkColor,
           clipPath: "polygon(100% 100%, 100% 0, 0 0)", // 삼각형 모양
         }}
       ></div>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,7 +3,23 @@ export default {
   mode: "jit",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        tinDownIn: {
+          "0%": {
+            opacity: "0",
+            transform: "scale(1) translateY(900%)",
+          },
+          "50%, 60%, 70%, 80%, 90%, 100%": {
+            opacity: "1",
+            transform: "scale(1) translateY(0)",
+          },
+        },
+      },
+      animation: {
+        tinDownIn: "tinDownIn 1s ease-out",
+      },
+    },
   },
   plugins: [],
 };
