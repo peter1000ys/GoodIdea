e93e9a84ef00af991e52663c2e139c56922c9d26
이주호 <zxxv1245@naver.com>
2024-10-30 11:51:02 +0900

    feat: 좌표값 추가

--- frontend/src/page/projectdetail/IdeaBoardPage.jsx
+++ frontend/src/page/projectdetail/IdeaBoardPage.jsx
@@ -3,23 +3,58 @@ import Header from "../../components/common/Header";
 import Sticker from "../../components/ideaboard/sticker";
 
 function IdeaBoardPage() {
+  const coordinates = [
+    { x: "0%", y: "5%", delay: 0 },
+    { x: "27%", y: "64%", delay: 100 },
+    { x: "80%", y: "56%", delay: 200 },
+    { x: "34%", y: "48%", delay: 300 },
+    { x: "67%", y: "12%", delay: 400 },
+    { x: "48%", y: "77%", delay: 500 },
+    { x: "3%", y: "56%", delay: 600 },
+    { x: "50%", y: "24%", delay: 700 },
+    { x: "77%", y: "34%", delay: 800 },
+    { x: "18%", y: "16%", delay: 900 },
+    { x: "29%", y: "23%", delay: 1000 },
+    { x: "67%", y: "7%", delay: 1100 },
+  ];
+
+  const colors = [
+    "#FFF8B7",
+    "#CFF3FF",
+    "#C6FFDC",
+    "#FCCDF7",
+    "#E8CAFC",
+    "#D2D2F8",
+  ];
+
+  const darkColors = [
+    "#E8DB78",
+    "#A4E1F5",
+    "#77FAA9",
+    "#EF99E6",
+    "#D2A3F1",
+    "#B1B1EE",
+  ];
+
   return (
     <>
       <Helmet>
-        <title> 아이디어보드 페이지</title>
+        <title>아이디어보드 페이지</title>
       </Helmet>
-      <div className="h-full w-full flex flex-col space-y-20">
+      <div className="h-full w-full relative overflow-hidden">
         <Header content="관통 프로젝트" />
-        <div className="flex justify-center items-center flex-row space-x-20">
-          <Sticker delay={0} />
-          <Sticker delay={200} />
-          <Sticker delay={400} />
-        </div>
-        <div className="flex justify-center items-center flex-row space-x-20">
-          <Sticker delay={600} />
-          <Sticker delay={800} />
-          <Sticker delay={1000} />
-        </div>
+
+        {/* Sticker 컴포넌트 좌표화 렌더링 */}
+        {coordinates.map(({ x, y, delay }, index) => (
+          <Sticker
+            key={index}
+            x={x}
+            y={y}
+            delay={delay}
+            color={colors[index % colors.length]}
+            darkColor={darkColors[index % darkColors.length]}
+          />
+        ))}
       </div>
     </>
   );
