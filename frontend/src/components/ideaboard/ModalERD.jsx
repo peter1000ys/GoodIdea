0238f37252eda95c360ac158d1738f23cf63cece
이주호 <zxxv1245@naver.com>
2024-10-31 16:14:32 +0900

    feat: 기능 추가

--- frontend/src/components/ideaboard/ModalERD.jsx
+++ frontend/src/components/ideaboard/ModalERD.jsx
@@ -1,2 +1,20 @@
-const ModalERD = () => <p className="p-4">ERD 내용입니다...</p>;
+import React, { Suspense } from "react";
+
+const ERDDrawing = React.lazy(() => import("../../components/erd/ERDDrawing"));
+// import ERDDrawing from "../../components/erd/ERDDrawing";
+
+const ModalERD = () => {
+  return (
+    <>
+      <div className="h-full w-full flex flex-col">
+        <div className="flex-1 flex">
+          <Suspense fallback={<div>로딩 중...</div>}>
+            <ERDDrawing />
+          </Suspense>
+        </div>
+      </div>
+    </>
+  );
+};
+
 export default ModalERD;
--- frontend/src/components/ideaboard/StickerModal.jsx
+++ frontend/src/components/ideaboard/StickerModal.jsx
@@ -109,7 +109,7 @@ const StickerModal = ({ closeModal, selectedSticker }) => {
           {renderContent()}
         </div>
         <button
-          className="absolute text-3xl top-16 right-7 text-gray-500 hover:text-gray-700"
+          className="absolute text-3xl top-16 right-7 text-gray-500 hover:text-gray-700 z-[100]"
           onClick={closeModal}
         >
           <big>&times;</big>
