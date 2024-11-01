73223acaf4ebe5ea73d6f80e258747dc8604c29b
이주호 <zxxv1245@naver.com>
2024-11-01 15:53:32 +0900

    feat: isLogin, userInfo 수정 및 호출

--- frontend/src/page/LoginPage.jsx
+++ frontend/src/page/LoginPage.jsx
@@ -1,33 +1,54 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { createAuthAxiosInstance } from "../api/http-commons/authAxios";
+import { useUserStore } from "../store/useUserStore";
+import axios from "axios";
 
 const LoginPage = () => {
   const navigate = useNavigate();
+  const setLogin = useUserStore((state) => state.setLogin);
 
   useEffect(() => {
-    // URL에서 토큰 추출
-    const urlParams = new URLSearchParams(window.location.search);
-    const accessToken = urlParams.get("accessToken");
-    const refreshToken = urlParams.get("refreshToken");
+    const handleLogin = async () => {
+      // URL에서 토큰 추출
+      const urlParams = new URLSearchParams(window.location.search);
+      const accessToken = urlParams.get("accessToken");
+      const refreshToken = urlParams.get("refreshToken");
+      if (accessToken && refreshToken) {
+        // 토큰을 세션 스토리지에 저장
+        sessionStorage.setItem("accessToken", accessToken);
+        sessionStorage.setItem("refreshToken", refreshToken);
+        console.log("Access Token:", accessToken);
+        console.log("Refresh Token:", refreshToken);
+        // 인증된 axios 인스턴스 생성
+        createAuthAxiosInstance();
+        try {
+          // 프로필 정보 요청
+          const profileResponse = await axios.get(
+            `https://oracle1.mypjt.xyz/api/v1/user/profile`,
+            {
+              headers: {
+                Authorization: `Bearer ${accessToken}`,
+              },
+            }
+          );
+          console.log("정보:", profileResponse.data);
+          const userInfo = profileResponse.data.data;
+          setLogin(userInfo); // userInfo 저장
 
-    if (accessToken && refreshToken) {
-      // 토큰을 세션 스토리지에 저장
-      sessionStorage.setItem("accessToken", accessToken);
-      sessionStorage.setItem("refreshToken", refreshToken);
-      console.log("Access Token:", accessToken);
-      console.log("Refresh Token:", refreshToken);
+          // 메인 페이지로 리디렉트
+          navigate("/");
+        } catch (error) {
+          console.error("프로필 정보 가져오기 실패:", error);
+        }
+      } else {
+        console.log("리디렉트된 URL에 토큰이 없습니다.");
+      }
+    };
+    handleLogin();
+  }, [navigate, setLogin]);
 
-      // 메인 페이지로 리디렉트
-      navigate("/");
-    } else {
-      console.log("리디렉트된 URL에 토큰이 없습니다.");
-    }
-  }, [navigate]);
-  return (
-    <>
-      <h1>Login Page</h1>
-    </>
-  );
+  return <>Login Page</>;
 };
 
 export default LoginPage;
