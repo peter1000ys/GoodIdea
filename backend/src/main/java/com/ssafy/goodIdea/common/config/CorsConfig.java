b8ec25a505a92dff29c28fb267fad4e9cc9421c7
황우성 <asdf1916@naver.com>
2024-11-05 13:34:03 +0900

    fix: CORS ORIGIN ADD

--- backend/src/main/java/com/ssafy/goodIdea/common/config/CorsConfig.java
+++ backend/src/main/java/com/ssafy/goodIdea/common/config/CorsConfig.java
@@ -16,6 +16,7 @@ public class CorsConfig {
         config.setAllowCredentials(true); // 내 서버가 응답을 할 때 json을 자바스크립트에서 처리할 수 있는지 설정하는 것
         config.addAllowedOrigin("http://loaclhost:8080");
         config.addAllowedOrigin("http://loaclhost:3000");
+        config.addAllowedOrigin("http://loaclhost:5173");
         config.addAllowedOrigin("https://oracle1.mypjt.xyz");
         config.addAllowedOrigin("https://lab.ssafy.com");
         config.addAllowedHeader("*");   // 모든 header에게 응답을 허용하겠다.
