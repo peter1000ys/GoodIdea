be0018a6ead845b390be711fc20a9926360affff
WOOSEONG-1 <asdf1916@naver.com>
2024-11-10 22:07:36 +0900

    fix

--- fastapi/crawling/main.py
+++ fastapi/crawling/main.py
@@ -27,6 +27,6 @@ async def start_news_crawling():
 async def all_news_crawling():
     try:
         handle_crawl_news_all_request()
-        return {"status": "크롤링 작업이 백그라운드에서 시작되었습니다."}
+        return {"status": "크롤링 작업이 시작되었습니다."}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
