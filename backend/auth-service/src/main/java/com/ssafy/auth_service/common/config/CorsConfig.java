3fa7fbf95a47825f12bbf47c1d8127b1046785c0
CheonYoSeong <gwpeter1000@naver.com>
2024-11-11 11:53:49 +0900

    new: CORS

--- backend/auth-service/src/main/java/com/ssafy/auth_service/common/config/CorsConfig.java
+++ backend/auth-service/src/main/java/com/ssafy/auth_service/common/config/CorsConfig.java
@@ -16,7 +16,9 @@ public class CorsConfig {
         config.setAllowCredentials(true); // 내 서버가 응답을 할 때 json을 자바스크립트에서 처리할 수 있는지 설정하는 것
         config.addAllowedOrigin("http://loaclhost:8000");
         config.addAllowedOrigin("http://loaclhost:3000");
+        config.addAllowedOrigin("http://loaclhost:5173");
         config.addAllowedOrigin("https://oracle1.mypjt.xyz");
+        config.addAllowedOrigin("https://goodidea.world");
         config.addAllowedOrigin("https://lab.ssafy.com");
         config.addAllowedHeader("*");   // 모든 header에게 응답을 허용하겠다.
         config.addAllowedMethod("*"); // 모든 POST, GET, PUT, DELETE, PATCH 요청 허용
