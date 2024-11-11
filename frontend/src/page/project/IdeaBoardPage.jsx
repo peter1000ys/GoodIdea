d8bd22aa1f3a00f21ae09c5a21c9ef321d3fb434
이주호 <zxxv1245@naver.com>
2024-11-11 15:14:17 +0900

    chore: 더미 변수 분리

--- frontend/src/page/project/IdeaBoardPage.jsx
+++ frontend/src/page/project/IdeaBoardPage.jsx
@@ -5,6 +5,7 @@ import StickerModal from "../../components/ideaboard/StickerModal";
 import DefaultButton from "../../components/common/DefaultButton";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import { DndProvider } from "react-dnd";
+import { coordinates as initCoordinates } from "../../components/ideaboard/variable";
 
 function IdeaBoardPage() {
   const [selectedSticker, setSelectedSticker] = useState(null); // 선택된 스티커
@@ -64,91 +65,7 @@ function IdeaBoardPage() {
   // };
 
   // 첫 6개의 섹션 랜덤 좌표 + 나머지 전체 범위 랜덤 좌표
-  const [coordinates, setCoordinates] = useState([
-    // ...generateSectionCoordinates(),
-    // ...generateRandomCoordinates(2),
-    {
-      id: 1,
-      x: "4%",
-      y: "0%",
-      delay: 0,
-      color: "#FFF8B7",
-      darkColor: "#E8DB78",
-      animation: "animate-tinDownIn",
-    },
-    {
-      id: 2,
-      x: "30%",
-      y: "8%",
-      delay: 100,
-      color: "#CFF3FF",
-      darkColor: "#A4E1F5",
-      animation: "animate-tinUpIn",
-    },
-    {
-      id: 3,
-      x: "67%",
-      y: "20%",
-      delay: 200,
-      color: "#C6FFDC",
-      darkColor: "#77FAA9",
-      animation: "animate-tinRightIn",
-    },
-    {
-      id: 4,
-      x: "8%",
-      y: "70%",
-      delay: 300,
-      color: "#FCCDF7",
-      darkColor: "#EF99E6",
-      animation: "animate-tinLeftIn",
-    },
-    {
-      id: 5,
-      x: "33%",
-      y: "55%",
-      delay: 400,
-      color: "#E8CAFC",
-      darkColor: "#D2A3F1",
-      animation: "animate-tinDownIn",
-    },
-    {
-      id: 6,
-      x: "86%",
-      y: "41%",
-      delay: 500,
-      color: "#D2D2F8",
-      darkColor: "#B1B1EE",
-      animation: "animate-tinUpIn",
-    },
-    {
-      id: 7,
-      x: "56%",
-      y: "54%",
-      delay: 600,
-      color: "#FFF8B7",
-      darkColor: "#E8DB78",
-      animation: "animate-tinRightIn",
-    },
-    {
-      id: 8,
-      x: "12%",
-      y: "38%",
-      delay: 700,
-      color: "#CFF3FF",
-      darkColor: "#A4E1F5",
-      animation: "animate-tinLeftIn",
-    },
-    {
-      id: 9,
-      x: "40%",
-      y: "71%",
-      delay: 800,
-      color: "#C6FFDC",
-      darkColor: "#77FAA9",
-      animation: "animate-tinDownIn",
-    },
-  ]);
+  const [coordinates, setCoordinates] = useState(initCoordinates);
 
   useEffect(() => {
     const handleKeyDown = (e) => {
@@ -326,6 +243,7 @@ function IdeaBoardPage() {
                   color={color}
                   darkColor={darkColor}
                   animation={animation}
+                  containerRef={containerRef} // 컨테이너 참조 전달
                   isSelected={coordinates[index] === selectedSticker}
                   onClick={() => handleStickerClick(index)}
                   onMoveSticker={handleMoveSticker}
@@ -335,7 +253,7 @@ function IdeaBoardPage() {
                     className="absolute flex flex-row items-center space-x-2 z-10"
                     style={{
                       top: "-1.2rem",
-                      left: "5rem",
+                      left: "4.5rem",
                       transform: "translate(-50%, -50%)",
                     }}
                   >
