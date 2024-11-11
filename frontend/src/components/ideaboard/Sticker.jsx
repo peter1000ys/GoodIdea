a2570c60f848f3c50686e4a68f4b6c6eb01a2f90
이주호 <zxxv1245@naver.com>
2024-11-11 21:25:12 +0900

    feat: 스티커 내용 serviceName이랑 introduction 로 채움, 없을 시 입력해달라는 문구 / prop 전부 분해해서 받던거 한번에 받음

--- frontend/src/components/ideaboard/Sticker.jsx
+++ frontend/src/components/ideaboard/Sticker.jsx
@@ -1,30 +1,45 @@
-const Sticker = ({ x, y, color, darkColor, onClick, animation }) => {
+const Sticker = ({ coordinate, onClick }) => {
   return (
     <div
-      className={`relative w-36 h-36 cursor-pointer transition-all z-10 ${animation}`}
+      className={`relative w-36 h-36 cursor-pointer transition-all z-10 ${coordinate.animation}`}
       style={{
-        left: x,
-        top: y,
+        left: `${coordinate.x}%`,
+        top: `${coordinate.y}%`,
         position: "absolute",
       }}
       onClick={onClick}
     >
       <div
         className="w-36 h-[120px] overflow-hidden"
-        style={{ backgroundColor: color }}
+        style={{ backgroundColor: coordinate.color }}
       >
-        <div className="p-4">
-          <p className="text-gray-700 text-xs">메모 내용을 입력하세요...</p>
+        <div className="px-4 py-2">
+          {coordinate.serviceName ? (
+            <h4 className="text-white text-lg font-semibold mb-2">
+              {coordinate.serviceName}
+            </h4>
+          ) : (
+            <p className="text-gray-700 text-[8px]">서비스 명을 입력해주세요</p>
+          )}
+        </div>
+        <div className="px-2">
+          {coordinate.introduction ? (
+            <h4 className="text-white text-lg font-semibold mb-2">
+              {coordinate.introduction}
+            </h4>
+          ) : (
+            <p className="text-gray-700 text-xs">서비스 소개를 입력해주세요</p>
+          )}
         </div>
       </div>
       <div
         className="w-[120px] h-6 ml-auto mr-0"
-        style={{ backgroundColor: color }}
+        style={{ backgroundColor: coordinate.color }}
       ></div>
       <div
         className="absolute bottom-0 left-0 w-6 h-6"
         style={{
-          backgroundColor: darkColor,
+          backgroundColor: coordinate.darkColor,
           clipPath: "polygon(100% 100%, 100% 0, 0 0)",
         }}
       ></div>
