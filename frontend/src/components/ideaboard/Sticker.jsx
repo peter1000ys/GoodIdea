3baf5d0f82c50366bcb0b6f3e9ca8a3470e758a6
이주호 <zxxv1245@naver.com>
2024-11-12 10:15:15 +0900

    feat:  css 수정

--- frontend/src/components/ideaboard/Sticker.jsx
+++ frontend/src/components/ideaboard/Sticker.jsx
@@ -1,7 +1,7 @@
 const Sticker = ({ coordinate, onClick }) => {
   return (
     <div
-      className={`relative w-36 h-36 cursor-pointer transition-all z-10 ${coordinate.animation}`}
+      className={`relative w-[100px] h-[100px] cursor-pointer transition-all z-10 ${coordinate.animation}`}
       style={{
         left: `${coordinate.x}%`,
         top: `${coordinate.y}%`,
@@ -10,34 +10,34 @@ const Sticker = ({ coordinate, onClick }) => {
       onClick={onClick}
     >
       <div
-        className="w-36 h-[120px] overflow-hidden"
+        className="w-[100px] h-[84px] overflow-hidden"
         style={{ backgroundColor: coordinate.color }}
       >
-        <div className="px-4 py-2">
+        <div className="px-2 py-2">
           {coordinate.serviceName ? (
-            <h4 className="text-white text-lg font-semibold mb-2">
-              {coordinate.serviceName}
-            </h4>
+            <p className="text-gray-700 text-[8px]">{coordinate.serviceName}</p>
           ) : (
             <p className="text-gray-700 text-[8px]">서비스 명을 입력해주세요</p>
           )}
         </div>
         <div className="px-2">
           {coordinate.introduction ? (
-            <h4 className="text-white text-lg font-semibold mb-2">
+            <p className="text-gray-700 text-[10px]">
               {coordinate.introduction}
-            </h4>
+            </p>
           ) : (
-            <p className="text-gray-700 text-xs">서비스 소개를 입력해주세요</p>
+            <p className="text-gray-700 text-[10px]">
+              서비스 소개를 입력해주세요
+            </p>
           )}
         </div>
       </div>
       <div
-        className="w-[120px] h-6 ml-auto mr-0"
+        className="w-[84px] h-[16px] ml-auto mr-0"
         style={{ backgroundColor: coordinate.color }}
       ></div>
       <div
-        className="absolute bottom-0 left-0 w-6 h-6"
+        className="absolute bottom-[0.4px] left-[0.1px] w-[16px] h-[16px]"
         style={{
           backgroundColor: coordinate.darkColor,
           clipPath: "polygon(100% 100%, 100% 0, 0 0)",
