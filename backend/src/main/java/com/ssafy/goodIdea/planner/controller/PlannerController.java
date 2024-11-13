9c588756275174d5a0e87977b5d5c0e50a2ecf09
김태연 <kam_ta_yon@naver.com>
2024-11-13 23:46:50 +0900

    [FULL]feat: crdt 테스트용(기획서만)

--- backend/src/main/java/com/ssafy/goodIdea/planner/controller/PlannerController.java
+++ backend/src/main/java/com/ssafy/goodIdea/planner/controller/PlannerController.java
@@ -1,24 +1,13 @@
 package com.ssafy.goodIdea.planner.controller;
 
+import com.ssafy.goodIdea.planner.dto.request.PlannerUpdateRequestDto;
 import com.ssafy.goodIdea.planner.dto.response.PlannerResponseDto;
 import com.ssafy.goodIdea.planner.dto.response.PlannerUpdateResponseDto;
-import com.ssafy.goodIdea.planner.entity.Planner;
 import com.ssafy.goodIdea.planner.service.PlannerService;
 import lombok.RequiredArgsConstructor;
 
-import org.springframework.messaging.handler.annotation.DestinationVariable;
-import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.web.bind.annotation.*;
-import com.ssafy.goodIdea.common.dto.DocumentOperationDto;
 import com.ssafy.goodIdea.common.exception.ApiResponse;
-import com.ssafy.goodIdea.common.exception.BaseException;
-import com.ssafy.goodIdea.common.exception.ErrorType;
-
-import java.util.Map;
-import java.util.HashMap;
-import java.util.UUID;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RestController
 @RequiredArgsConstructor
@@ -27,12 +16,21 @@ public class PlannerController {
     private final PlannerService plannerService;
 
     /*
-     * 플래너 조회
+     * 기획서 조회
      */
     @GetMapping("/{ideaId}")
     public ApiResponse<PlannerResponseDto> getPlanner(@PathVariable(name = "ideaId") Long ideaId) {
-        // PlannerService를 통해 플래너 조회
+        // PlannerService를 통해 기획서 조회
         PlannerResponseDto plannerResponseDto = plannerService.getPlanner(ideaId);
         return ApiResponse.ok(plannerResponseDto);
+    }
+
+    /*
+     * 기획서 수정
+     */
+    @PutMapping("/{ideaId}")
+    public ApiResponse<PlannerUpdateResponseDto> updatePlanner(@PathVariable(name = "ideaId") Long ideaId, @RequestBody PlannerUpdateRequestDto plannerUpdateRequestDto) {
+        PlannerUpdateResponseDto plannerUpdateResponseDto = plannerService.updatePlanner(ideaId, plannerUpdateRequestDto);
+        return ApiResponse.ok(plannerUpdateResponseDto);
     }
 }
