80b851c86d1d202c817f2e937e5588f6c4567c80
한세훈 <atdark04@gmail.com>
2024-11-14 16:58:17 +0900

    feat: crdt 2단계 완료

--- frontend/src/page/project/ProposalPage.jsx
+++ frontend/src/page/project/ProposalPage.jsx
@@ -1,16 +1,26 @@
 // ProposalPage.jsx
 import React from "react";
 import { RoomProvider } from "@liveblocks/react";
-import { useParams } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import ProposalEditor from "./ProposalEditor"; // ProposalEditor 컴포넌트 import
+import { ClientSideSuspense } from "@liveblocks/react";
+
+const Room = ({ children }) => {
+  const { pathname } = useLocation();
+  return (
+    <RoomProvider id={`proposal-room-${pathname}`}>
+      <ClientSideSuspense fallback={<div>loading...</div>}>
+        {() => children}
+      </ClientSideSuspense>
+    </RoomProvider>
+  );
+};
 
 export default function ProposalPage() {
-  const { ideaId } = useParams(); // URL에서 ideaId 파라미터 가져오기
-
   return (
     // RoomProvider로 ProposalEditor를 감싸서 Liveblocks 방 기능을 제공합니다.
-    <RoomProvider id={`proposal-room-${ideaId}`}>
+    <Room>
       <ProposalEditor />
-    </RoomProvider>
+    </Room>
   );
 }
