31195694444faf8811b64969660a1d263f531909
한세훈 <atdark04@gmail.com>
2024-11-18 14:23:01 +0900

    feat: Brotli 비교 후

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,12 +5,12 @@ import compression from "vite-plugin-compression";
 export default defineConfig({
   plugins: [
     react(),
-    // compression({
-    //   algorithm: "brotliCompress", // Brotli 압축 알고리즘 사용
-    //   ext: ".br", // 확장자 설정
-    //   threshold: 10240, // 압축할 파일 크기 (바이트 단위), 10KB 이상의 파일만 압축
-    //   deleteOriginFile: false, // 원본 파일 유지
-    // }),
+    compression({
+      algorithm: "brotliCompress", // Brotli 압축 알고리즘 사용
+      ext: ".br", // 확장자 설정
+      threshold: 10240, // 압축할 파일 크기 (바이트 단위), 10KB 이상의 파일만 압축
+      deleteOriginFile: false, // 원본 파일 유지
+    }),
   ],
   server: {
     host: "0.0.0.0",
